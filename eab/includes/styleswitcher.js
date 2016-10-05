2735ef1e0e760a7c0266c4d5a65193b05d975850
Nick Freear <nfreear@yahoo.co.uk>
2016-10-05 09:26:57 +0100

    `styleswitcher` Javascript - jshint/ move globals into `EAB` global

--- eab/includes/styleswitcher.js
+++ eab/includes/styleswitcher.js
@@ -5,71 +5,82 @@
  *
  * Src: http://alistapart.com/stories/alternate, Sowden & Koch, 126/2001-11 (2003-07-21).
  */
-function setActiveStyleSheet(title) {
+
+(function (window) {
+
+  'use strict';
+
+  var EAB = window.EAB = {}
+    , document = window.document;
+
+EAB.setActiveStyleSheet = function (title) {
   var i, a, main;
-  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
-    if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title")) {
+  for(i = 0; (a = document.getElementsByTagName("link")[ i ]); i++) {
+    if(a.getAttribute("rel").indexOf("style") !== -1 && a.getAttribute("title")) {
       a.disabled = true;
-      if(a.getAttribute("title") == title) a.disabled = false;
+      if(a.getAttribute("title") === title) { a.disabled = false; }
     }
   }
 }
 
-function getActiveStyleSheet() {
+EAB.getActiveStyleSheet = function () {
   var i, a;
-  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
-    if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title") && !a.disabled) return a.getAttribute("title");
+  for(i = 0; (a = document.getElementsByTagName("link")[ i ]); i++) {
+    if(a.getAttribute("rel").indexOf("style") !== -1 && a.getAttribute("title") && ! a.disabled)
+      { return a.getAttribute("title"); }
   }
   return null;
 }
 
-function getPreferredStyleSheet() {
+EAB.getPreferredStyleSheet = function () {
   // Return a constant - style sheet 'title'
   return "Graphic";
-  
+
   /*var i, a;
-  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
-    if(a.getAttribute("rel").indexOf("style") != -1
-       && a.getAttribute("rel").indexOf("alt") == -1
+  for(i = 0; (a = document.getElementsByTagName("link")[ i ]); i++) {
+    if(a.getAttribute("rel").indexOf("style") !== -1
+       && a.getAttribute("rel").indexOf("alt") === -1
        && a.getAttribute("title")
        ) return a.getAttribute("title");
   }
   return null;*/
 }
 
-function createCookie(name,value,days) {
+EAB.createCookie = function (name, value, days) {
+  var expires;
   if (days) {
     var date = new Date();
-    date.setTime(date.getTime()+(days*24*60*60*1000));
-    var expires = "; expires="+date.toGMTString();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toGMTString();
   }
-  else expires = "";
-  document.cookie = name+"="+value+expires+"; path=/";
+  else { expires = ""; }
+  document.cookie = name + "=" + value + expires + "; path=/";
 }
 
-function readCookie(name) {
+EAB.readCookie = function (name) {
   var nameEQ = name + "=";
   var ca = document.cookie.split(';');
-  for(var i=0;i < ca.length;i++) {
-    var c = ca[i];
-    while (c.charAt(0)==' ') c = c.substring(1,c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[ i ];
+    while (c.charAt(0) === ' ') { c = c.substring(1, c.length); }
+    if (c.indexOf(nameEQ) === 0) { return c.substring(nameEQ.length, c.length); }
   }
   return null;
 }
 
-window.onload = function(e) {
-  var cookie = readCookie("style");
-  var title = cookie ? cookie : getPreferredStyleSheet();
-  setActiveStyleSheet(title);
+window.onload = function (ev) {
+  var cookie = EAB.readCookie("style");
+  var title = cookie ? cookie : EAB.getPreferredStyleSheet();
+  EAB.setActiveStyleSheet(title);
 }
 
-window.onunload = function(e) {
-  var title = getActiveStyleSheet();
-  createCookie("style", title, 365);
+window.onunload = function (ev) {
+  var title = EAB.getActiveStyleSheet();
+  EAB.createCookie("style", title, 365);
 }
 
-var cookie = readCookie("style");
-var title = cookie ? cookie : getPreferredStyleSheet();
-setActiveStyleSheet(title);
+var cookie = EAB.readCookie("style");
+var title = cookie ? cookie : EAB.getPreferredStyleSheet();
+EAB.setActiveStyleSheet(title);
 
+}(window));
