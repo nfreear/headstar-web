92133454418d6339ae13f7242d26ebac3ecd900a
Nick Freear <nfreear@yahoo.co.uk>
2017-11-27 18:13:03 +0000

    Fix/ extend `index.json` PHP script and JSON file - order "DESC"

--- perl/index-json.php
+++ perl/index-json.php
@@ -6,16 +6,27 @@
  * @copyright Nick Freear, 26-November-2017.
  */
 
+define( 'IS_ASCEND', false );
 define( 'START_YEAR', 2000 );
 define( 'END_YEAR', 2017 );
 define( 'DIR', __DIR__ . '/../eab/issues/' );
 define( 'INDEX_JSON', __DIR__ . '/../eab/index.json' );
 define( 'ISSUE_REGEX', '/\n([\*-] )?ISSUE (?P<issue>\d+),/s' );
+define( 'TXTFILE_REGEX', '/^(?P<month>jan|[a-z]{3})20\d\d\.txt$/' );
+define( 'EDITOR_REGEX', '/Editor:[ ]+(?P<ed>Tristan Parker|Dan-J-XX)/' );
+define( 'TENS_ISSUE', 36 );  // http://headstar.com/ten
+define( 'TENS_DATE', '2002-12-01' );  // December 2002.
+define( 'NOMINAL_PUBLISH_DAY', 20 );  // Day of month.
+define( 'MONTHS', 'jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec' );
 
-$archive = [];
+require_once 'ReversibleForLoop.php';
+
+$issue_archive = [];
 $count = 0;
 
-for ($year = START_YEAR; $year <= END_YEAR; $year++) {
+$for = new Nfreear\ReversibleForLoop(START_YEAR, END_YEAR + 1, IS_ASCEND);
+
+$result = $for->loop(function ($year) use (&$count, &$issue_archive) {
 
   $directory = DIR . $year;
   $files = scandir( $directory );
@@ -23,7 +34,7 @@ for ($year = START_YEAR; $year <= END_YEAR; $year++) {
   $year_archive = [];
 
   foreach ($files as $file) {
-    if ( ! preg_match( '/.+20\d\d\.txt/', $file ) ) {
+    if ( ! preg_match( TXTFILE_REGEX, $file ) ) {
       continue;
     }
 
@@ -42,24 +53,38 @@ for ($year = START_YEAR; $year <= END_YEAR; $year++) {
       echo 'E' . $year;
     }
 
+    $editor = preg_match( EDITOR_REGEX, $bulletin_text ) ? 'TP' : 'DJ';
+
     $year_archive[ $issue_num ] = [
       'issue' => (int) $issue_num,
       'text_file' => $file,
       'html_file' => str_replace( '.txt', '.html', $file ),
+      'editor' => $editor,
     ];
 
     $count++;
   }
 
-  $archive[ $year ] = $year_archive;
-}
+  $s_result = IS_ASCEND ? ksort( $year_archive ) : krsort( $year_archive );
+
+  $issue_archive[ $year ] = $year_archive;
+});
+
+// var_dump( $result );
 
 $archive = [
   'title' => 'E-Access Bulletin archive.',
-  'time' => date( 'c' ),
+  'build_time' => date( 'c' ),
   'base_url' => 'http://headstar.com/eab/issues/{year}/{file}',
+  'editors' => [
+    'DJ' => 'Dan Jellinek',
+    'TP' => 'Tristan Parker',
+  ],
+  'tens_issue' => TENS_ISSUE,
+  'tens_date' => TENS_DATE,
   'issue_count' => $count,
-  'issues' => $archive,
+  'order_by' => IS_ASCEND ? 'ASC' : 'DESC',
+  'issues' => $issue_archive,
 ];
 
 $bytes = file_put_contents( INDEX_JSON, json_encode( $archive , JSON_PRETTY_PRINT ));
